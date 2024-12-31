529b1b3fdc432733639d1d7dfc90f94099951ea2
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-12-31 16:04:01 +0200

    chore: add enums definitions

---
+++ front-end/app/_enums/index.ts
@@ -0,0 +1,17 @@
+
+export enum Asset {
+    EUR = "EUR",
+    BTC = "BTC",
+    USDC = "USDC",
+    USDT = "USDT",
+    ETH = "ETH",
+    BNB = "BNB",
+    POL = "POL"
+}
+
+export enum ActionType {
+    SET_SOURCE,
+    SET_DESTINATION,
+    SET_SOURCE_AMOUNT,
+    SET_DESTINATION_AMOUNT,
+}
