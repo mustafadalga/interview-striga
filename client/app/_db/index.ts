64ee90cbbabc808343b2474df884be7fba3285e8
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-12-31 18:01:54 +0200

    fix: optimize MongoDB connection and improve API error handling

--- client/app/_db/index.ts
+++ client/app/_db/index.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 import { env } from "@/_constants";
 
+let isConnected = false;
+
 export async function connectDB() {
+    if (isConnected) {
+        return; // Use existing connection
+    }
+
     try {
+
         if (mongoose.connection.readyState !== 1) {
             await mongoose.connect(env.MONGODB_URL);
             console.log("MongoDB connected");
         }
+
+        isConnected = true;
+
     } catch (error) {
         console.error("MongoDB connection error:", error);
         throw error; // Propagate error to stop execution
