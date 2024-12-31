5151b058e785021975f4a2f44e29e79cdb0ce6c4
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-12-31 16:08:15 +0200

    feat: define Rate model using mongoose

---
+++ front-end/app/_models/Rate.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Model } from "mongoose";
+import { Rates } from "@/_types";
+
+interface RateDocument extends Document {
+    _id: mongoose.Schema.Types.ObjectId
+    data: Rates;
+}
+
+interface RateModel extends Model<RateDocument> {}
+
+
+const schema = new mongoose.Schema<RateDocument>({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    data: {
+        type: Object,
+        required: true,
+    },
+});
+
+const Rate = mongoose.models.Rate as RateModel || mongoose.model<RateDocument, RateModel>("Rate", schema);
+
+export default Rate
